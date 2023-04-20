3d4df75992cb755f4525a3e888b58aee7a67e7c7
feixianxing <1663521808@qq.com>
2023-04-20 22:49:35 +0800

    feat: 初始化代码模板

---
+++ README.md
@@ -0,0 +1,18 @@
+# Vue 3 + Vite + Electron
+
+* 自用模板，用于快速开发基于Vue3和electron的桌面应用程序.
+* 使用Vue3+Vite+Electron
+
+* 目前需要手动更新
+```
+npm run build
+```
+
+```
+npm run start
+```
+
+* 后续再更新
+
+> 作者
+> [@feixianxing](https://github.com/feixianxing)🦊
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p class="demo">Hello Electron!😎</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.demo{
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
